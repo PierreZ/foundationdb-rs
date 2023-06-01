a455ca5444315c47ad40f5cdfef9fa0f3014abfb
Pierre Zemb <pierre.zemb@clever-cloud.com>
2023-06-01 13:19:27 +0200

    chore(all): bump dependencies

--- foundationdb-macros/src/lib.rs
+++ foundationdb-macros/src/lib.rs
@@ -1,9 +1,23 @@
 //! Macro definitions used to maintain the crate
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote, ToTokens};
 use std::collections::HashMap;
 use syn::__private::TokenStream2;
-use syn::{AttributeArgs, Item, Lit, Meta, NestedMeta};
+use syn::parse::{Parse, ParseStream};
+use syn::punctuated::Punctuated;
+use syn::{Attribute, Expr, Item, Lit, Meta, Token};
+
+struct ParsedStruct {
+    attrs: Vec<Attribute>,
+}
+
+impl Parse for ParsedStruct {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(ParsedStruct {
+            attrs: input.call(Attribute::parse_inner)?,
+        })
+    }
+}
 
 /// Allow to compute the range of supported api versions for a functionality.
 ///
@@ -19,19 +33,24 @@ use syn::{AttributeArgs, Item, Lit, Meta, NestedMeta};
 /// `#[cfg(any(feature = "fdb-5_1", feature = "fdb-5_2", feature = "fdb-6_0"))]`
 #[proc_macro_attribute]
 pub fn cfg_api_versions(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(item as Item);
-    let attributes = syn::parse_macro_input!(attr as AttributeArgs);
+    cfg_api_versions_internal(attr, item)
+}
 
-    let (minimum_version, maximum_version) = parse_version_arguments(&attributes);
+fn cfg_api_versions_internal(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(item as Item);
+    let attributes = syn::parse_macro_input!(attr as ParsedStruct);
+
+    let (minimum_version, maximum_version) = parse_version_arguments(attributes.attrs);
     generate_feature_range(&input, minimum_version, maximum_version)
 }
 
 /// Search for a required min and an optional max in the attributes
-fn parse_version_arguments(attributes_args: &AttributeArgs) -> (i32, Option<i32>) {
+fn parse_version_arguments(attributes_args: Vec<Attribute>) -> (i32, Option<i32>) {
     let min = attributes_args
         .iter()
         .find_map(|attribute| find_attribute(attribute, "min"))
         .expect("Macro is expecting at least a 'min' argument");
+
     let max = attributes_args
         .iter()
         .find_map(|attribute| find_attribute(attribute, "max"));
@@ -40,25 +59,28 @@ fn parse_version_arguments(attributes_args: &AttributeArgs) -> (i32, Option<i32>
 }
 
 /// given an attribute's key, returns the associated i32, or None.
-fn find_attribute(attribute: &NestedMeta, key: &str) -> Option<i32> {
-    match attribute {
-        NestedMeta::Meta(Meta::NameValue(name_value)) => {
-            if name_value.path.is_ident(key) {
-                if let Lit::Int(attribute_value) = &name_value.lit {
-                    Some(
-                        attribute_value
-                            .base10_parse::<i32>()
-                            .expect("could not cast attribute to i32"),
-                    )
-                } else {
-                    None
+fn find_attribute(attribute: &Attribute, key: &str) -> Option<i32> {
+    if attribute.path().is_ident("cfg_api_versions") {
+        let nested = attribute
+            .parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)
+            .expect("cannot parse nested argument in procedure macro");
+        for meta in nested {
+            if let Meta::NameValue(name_value) = meta {
+                if name_value.path.is_ident(key) {
+                    if let Expr::Lit(attribute_expr) = &name_value.value {
+                        if let Lit::Int(attribute_value) = &attribute_expr.lit {
+                            return Some(
+                                attribute_value
+                                    .base10_parse::<i32>()
+                                    .expect("could not cast attribute to i32"),
+                            );
+                        }
+                    }
                 }
-            } else {
-                None
             }
         }
-        _ => None,
     }
+    None
 }
 
 fn generate_feature_range(
@@ -72,11 +94,12 @@ fn generate_feature_range(
             .map(|fdb_version| quote!(feature = #fdb_version))
             .collect();
 
-    quote!(
+    let result = quote!(
         #[cfg(any(#(#allowed_fdb_versions),*))]
         #input
-    )
-    .into()
+    );
+
+    result.into()
 }
 
 /// Given a range of version, this function will generate the appropriate macro text.
@@ -111,7 +134,8 @@ fn get_version_mapping() -> HashMap<String, i32> {
 
 #[cfg(test)]
 mod tests {
-    use crate::get_supported_feature_range;
+    use crate::{get_supported_feature_range, parse_version_arguments};
+    use syn::{parse_quote, Attribute};
 
     #[test]
     fn test_create_supported_list() {
@@ -147,5 +171,25 @@ mod tests {
         assert!(v.contains(&String::from("fdb-5_2")));
         assert!(v.contains(&String::from("fdb-5_1")));
         assert!(v.contains(&String::from("fdb-5_0")));
+    }
+
+    #[test]
+    fn test_version_extract_no_max() {
+        let attr: Attribute = parse_quote! {
+            #[cfg_api_versions(min = 620)]
+        };
+
+        let result = parse_version_arguments(vec![attr]);
+        assert_eq!(result, (620, None));
+    }
+
+    #[test]
+    fn test_version_extract_with_max() {
+        let attr: Attribute = parse_quote! {
+            #[cfg_api_versions(min = 620, max = 700)]
+        };
+
+        let result = parse_version_arguments(vec![attr]);
+        assert_eq!(result, (620, Some(700)));
     }
 }
